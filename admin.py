20023b98f9e72e1191aa4046d4ad5ea0a572b0d6
Ben O'Neill <270086445@yoobeestudent.ac.nz>
2024-05-17 10:47:03 +1200

    admin addition

---
+++ admin.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+conn = sqlite3.connect('admin.sqlite')
+
+c = conn.cursor()
+sql_querry = """CREATE TABLE admin account (
+                adminID integer PRIMARY KEY,
+                admin_account_name text,
+                admin_password text,
+                admin_access text,
+                )"""
+c.exucute(sql_querry)
