a22c0ccb0ddbdb942ce8c851641086b61028813c
Jacob <270137506@yoobeestudent.ac.nz>
2024-06-24 11:07:25 +1200

    I am genuinely stuck on how to make the search bar in Flask. Please help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
@@ -11,6 +12,24 @@ def db_connect():
     except sqlite3.Error as e:
         print(e)
     return conn
+
+
+@app.route('/add', methods=['POST', 'GET'])
+def add_movies():
+    if request.method == 'GET':
+        return 'movies'
+    elif request.method == 'POST':
+        conn = db_connect()
+        c = conn.cursor()
+        title = request.form['title']
+        year = request.form['year']
+        genre = request.form['genre']
+        director = request.form['director']
+        rating = request.form['rating']
+        sql_query = "INSERT INTO movies (title, year, genre, director, rating) VALUES (?,?,?,?,?)"
+        c.execute(sql_query, (title, year, genre, director, rating))
+        conn.commit()
+        return "Added movie!"
 
 
 def load_movies():
